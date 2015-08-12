4db250039f8ae771bbda92919032c37fab8914b8
Aysegul Yonet <aysegulyonet@gmail.com>
2015-08-11 18:47:14 -0700

    Another merge sort.

---
+++ app/scripts/mergeSort2.js
@@ -0,0 +1,24 @@
+var mergeArrays = function(arr1, arr2) {
+	var result = [];
+
+	if(arr1.length === 0 || arr2.length === 0) {
+		return arr1.concat(arr2);
+	} else {
+		var len = arr1.length + arr2.length;
+		var point1 = arr1[0];
+		var point2 = arr2[0];
+		var i = 0;
+		var j = 0;
+		while(result.length < len){
+			if(arr1[i] < arr2[j]){
+				result.push(arr1[i]);
+				i++;
+			} else {
+				result.push(arr2[j]);
+				j++;
+			}
+		}
+	}
+	return result;
+	
+}	
